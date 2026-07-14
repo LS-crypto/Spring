b0156116bb4d95bb0f01d9d4bff72c0c04f44979
李烁烁 <2575853136@qq.com>
2026-07-14 14:17:26 +0800

    refactor: 迁移测试文件至新包名 com.playmate - 删除旧 DemoApplicationTests - 创建 PlayMateApplicationTests - 移除项目规划文档(非代码文件)

--- playmate-backend/src/test/java/com/playmate/PlayMateApplicationTests.java
+++ playmate-backend/src/test/java/com/playmate/PlayMateApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.playmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PlayMateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
